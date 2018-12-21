0b8636f555abadf4e8c86e7e800e217b6e26fd06
gilvan <g.r.almeida@live.com>
2018-12-20 22:44:54 -0200

    Retrieving Fun Type Offers Through an Http Request

--- src/app/fun/fun.component.ts
+++ src/app/fun/fun.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {Offer} from '../shared/models/offer.model'
+import {OffersService} from '../shared/offers.service'
 
 @Component({
   selector: 'app-fun',
   templateUrl: './fun.component.html',
-  styleUrls: ['./fun.component.css']
+  styleUrls: ['./fun.component.css'],
+  providers: [
+    OffersService
+  ]
 })
 export class FunComponent implements OnInit {
 
-  constructor() { }
+  private funs: Array<Offer>
+
+  constructor(private offersService: OffersService) { }
 
   ngOnInit() {
+    this.offersService
+      .getOffersByCategory('fun')
+      .then((offers: Array<Offer>) => this.funs = offers)
   }
 
 }
