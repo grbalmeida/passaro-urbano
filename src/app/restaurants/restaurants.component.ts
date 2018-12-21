e82ac3b4668fc1c12f7da5edd421d8cd42401a3b
gilvan <g.r.almeida@live.com>
2018-12-20 22:28:11 -0200

    Retrieving Restaurant Type Offers Through an Http Request

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {OffersService} from '../shared/offers.service'
+import {Offer} from '../shared/models/offer.model'
 
 @Component({
   selector: 'app-restaurants',
   templateUrl: './restaurants.component.html',
-  styleUrls: ['./restaurants.component.css']
+  styleUrls: ['./restaurants.component.css'],
+  providers: [
+    OffersService
+  ]
 })
 export class RestaurantsComponent implements OnInit {
 
-  constructor() { }
+  private restaurants: Array<Offer>
+
+  constructor(private offersService: OffersService) { }
 
   ngOnInit() {
+    this.offersService
+      .getOffersByCategory('restaurants')
+      .then((offers: Array<Offer>) => this.restaurants = offers)
   }
 
 }
--- src/app/shared/offers.service.ts
+++ src/app/shared/offers.service.ts
@@ -4,11 +4,19 @@ import {Offer} from './models/offer.model'
 
 @Injectable()
 export class OffersService {
+    private url: string = 'http://localhost:3000/offers'
     constructor(private http: HttpClient) {}
 
     public getOffers(): Promise<Array<Offer>> {
         return this.http
-            .get('http://localhost:3000/offers?highlight=true')
+            .get(`${this.url}?highlight=true`)
+            .toPromise()
+            .then((response: any) => response)
+    }
+
+    public getOffersByCategory(category: string): Promise<Array<Offer>> {
+        return this.http    
+            .get(`${this.url}?category=${category}`)
             .toPromise()
             .then((response: any) => response)
     }
