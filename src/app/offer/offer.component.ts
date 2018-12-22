eea8ef78ee4c585fc2b992d3229408e8065fc2b8
gilvan <g.r.almeida@live.com>
2018-12-22 04:45:08 -0200

    Making Offer component dynamic

--- src/app/offer/offer.component.ts
+++ src/app/offer/offer.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router'
+import {OffersService} from '../shared/offers.service'
+import {Offer} from '../shared/models/offer.model'
 
 @Component({
   selector: 'app-offer',
   templateUrl: './offer.component.html',
-  styleUrls: ['./offer.component.css']
+  styleUrls: ['./offer.component.css'],
+  providers: [
+    OffersService
+  ]
 })
 export class OfferComponent implements OnInit {
+  public offer: Offer
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute, 
+    private offersService: OffersService
+  ) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.params['id']
+    this.offersService
+      .getOfferById(id)
+      .then((offer: Offer) => this.offer = offer)
   }
 
 }
--- src/app/shared/offers.service.ts
+++ src/app/shared/offers.service.ts
@@ -1,6 +1,7 @@
 import {HttpClient} from '@angular/common/http'
 import {Injectable} from '@angular/core'
 import {Offer} from './models/offer.model'
+import {API_URL} from '../app.api'
 
 @Injectable()
 export class OffersService {
@@ -9,15 +10,22 @@ export class OffersService {
 
     public getOffers(): Promise<Array<Offer>> {
         return this.http
-            .get(`${this.url}?highlight=true`)
+            .get(`${API_URL}?highlight=true`)
             .toPromise()
             .then((response: any) => response)
     }
 
     public getOffersByCategory(category: string): Promise<Array<Offer>> {
         return this.http    
-            .get(`${this.url}?category=${category}`)
+            .get(`${API_URL}?category=${category}`)
             .toPromise()
             .then((response: any) => response)
+    }
+
+    public getOfferById(id: number): Promise<Offer> {
+        return this.http
+            .get(`${API_URL}?id=${id}`)
+            .toPromise()
+            .then((response: any) => response.shift())
     }
 }
